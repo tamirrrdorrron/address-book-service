e6e57b92c96492e384f851f0e77b4517f6079c37
Tamir Doron <Tamir.Doron@reece.com.au>
2021-06-23 21:50:10 +1000

    add h2 in mem db

--- src/main/java/au/com/reece/addressbook/AddressBookApplication.java
+++ src/main/java/au/com/reece/addressbook/AddressBookApplication.java
@@ -1,5 +1,6 @@
 package au.com.reece.addressbook;
 
+import au.com.reece.addressbook.model.AddressBooks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AddressBookApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(AddressBookApplication.class, args);
 	}
 
---
+++ src/main/java/au/com/reece/addressbook/model/AddressBooks.java
@@ -0,0 +1,12 @@
+package au.com.reece.addressbook.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+public class AddressBooks {
+    List<AddressBook> addressBooks;
+}
